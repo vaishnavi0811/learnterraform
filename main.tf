f3bda37b7a020075236465ad54523b885866f2ee
M <VM13@its.jnj.com>
2021-05-03 01:42:55 -0400

    added subnet

--- main.tf
+++ main.tf
@@ -6,8 +6,15 @@ resource "azurerm_virtual_network" "samplevnet" {
     name = "azurevnet"
     location = "south india"
     resource_group_name = "Learning"
-    address_space = ["10.0.0.0/24"]
+    address_space = ["10.0.0.0/16"]
     tags = {
         environmet = "Test"
     }
+}
+
+resource "azurerm_subnet" "example" {
+    name = "subnet0"
+    resource_group_name = "Learning"
+    virtual_network_name = "azurerm_virtual_network.example.name"
+    address_prefixes = ["10.0.1.0/24"]
 }
