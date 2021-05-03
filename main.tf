a61eaa4232df49ed68b31048d4d4a28c40ded9b5
M <VM13@its.jnj.com>
2021-05-03 06:15:38 -0400

    Added NSG

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
 	 features {}
 }
 
-resource "azurerm_virtual_network" "samplevnet" {
+resource "azurerm_virtual_network" "vnet" {
     name = "testvnet"
     location = "south india"
     resource_group_name = "Learning"
@@ -13,9 +13,32 @@ resource "azurerm_virtual_network" "samplevnet" {
     }
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "subnet" {
     name = "subnet0"
     resource_group_name = "Learning"
-    virtual_network_name = azurerm_virtual_network.samplevnet.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes = ["20.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "azurensg" {
+    name = "nsg01"
+    location = azurerm_virtual_network.vnet.location
+    resource_group_name = azurerm_virtual_network.vnet.resource_group_name
+
+    security_rule {
+        name = "nsgrule1"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "*"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsgassociate" {
+  subnet_id = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.azurensg.id
 }
