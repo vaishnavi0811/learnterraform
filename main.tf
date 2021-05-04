2a8cdff03b3093103259eec52508e0ecd39d574e
M <VM13@its.jnj.com>
2021-05-04 07:24:57 -0400

    added another vnet for peering

--- main.tf
+++ main.tf
@@ -42,3 +42,35 @@ resource "azurerm_subnet_network_security_group_association" "nsgassociate" {
   subnet_id = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.azurensg.id
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+    name = "testvnet1"
+    location = "south india"
+    resource_group_name = azurerm_virtual_network.vnet.resource_group_name
+    address_space = ["30.0.0.0/16"]
+
+    tags = {
+        environmet = "Test"
+    }
+}
+
+resource "azurerm_subnet" "subnet0" {
+    name = "subnet1"
+    resource_group_name = azurerm_virtual_network.vnet.resource_group_name
+    virtual_network_name = azurerm_virtual_network.vnet1.name
+    address_prefixes = ["30.0.1.0/24"]
+}
+
+resource "azurerm_virtual_network_peering" "peer1" {
+  name                      = "peer1to2"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet1.id
+}
+
+resource "azurerm_virtual_network_peering" "peer2" {
+  name                      = "peer2to1"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet.id
+}
