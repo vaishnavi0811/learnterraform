a8df3730c15150447b9b10231f385119fe6e6f05
vaishnavi0811 <79398999+vaishnavi0811@users.noreply.github.com>
2021-05-07 08:21:32 -0400

    Update main.tf

--- createvm/vnet/main.tf
+++ createvm/vnet/main.tf
@@ -16,3 +16,12 @@ resource "azurerm_subnet" "subnet0" {
     address_prefixes = ["40.0.1.0/24"]
     depends_on = [azurerm_virtual_network.vnet0,azurerm_network_security_group.azurensg]
 }
+output "subnetid" {
+    value = azurerm_subnet.subnet0.id
+}
+output "rsgname" {
+    value = azurerm_virtual_network.vnet0.resource_group_name
+}
+output "location" {
+    value = azurerm_virtual_network.vnet0.location
+}
