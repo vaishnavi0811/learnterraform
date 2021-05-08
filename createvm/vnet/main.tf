d3f69857bc2790cb55623717ee937579effb5897
vaishnavi0811 <79398999+vaishnavi0811@users.noreply.github.com>
2021-05-08 10:04:32 -0400

    update depends on

--- createvm/vnet/main.tf
+++ createvm/vnet/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "subnet0" {
     resource_group_name = "Learning"
     virtual_network_name = azurerm_virtual_network.vnet0.name
     address_prefixes = ["40.0.1.0/24"]
-    depends_on = [azurerm_virtual_network.vnet0,azurerm_network_security_group.azurensg]
+    depends_on = [azurerm_virtual_network.vnet0]
 }
 output "subnetid" {
     value = azurerm_subnet.subnet0.id
